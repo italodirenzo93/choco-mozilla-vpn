5ec0e095e559fb022b7c53ff15929bf6da38f7fd
Italo Di Renzo <italodirenzo93@gmail.com>
2020-09-28 02:26:46 -0400

    Fix checksum

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -34,7 +34,7 @@ $packageArgs = @{
   # e.g. checksum -t sha256 -f path\to\file
   checksum       = ''
   checksumType   = 'sha256' #default is md5, can also be sha1, sha256 or sha512
-  checksum64     = '7ED88D4CCD3058B2ECA1E27B1681121C0D9A78D93B3550D1236F2868024AF22C'
+  checksum64     = '787D90C0BDF24E1B288137451A793A1EE968A7B2128CC86FF9FCAE43C264C3F8'
   checksumType64 = 'sha256' #default is checksumType
 
   # MSI
