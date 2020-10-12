ccd03412b26afb6e52bf5eff15c0d2b09822d2d1
Italo Di Renzo <i.direnzo@vehikl.com>
2020-10-12 17:46:51 -0400

    Remove non 64-bit urls and fields

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,20 +1,16 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$url = ''
 $url64 = 'https://vpn.mozilla.org/downloads/vpn/windows/v1.1/MozillaVPN.msi'
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   unzipLocation  = $toolsDir
   fileType       = 'MSI'
-  url            = $url
   url64bit       = $url64
 
   softwareName   = 'Mozilla VPN'
 
-  checksum       = ''
-  checksumType   = 'sha256'
   checksum64     = '787D90C0BDF24E1B288137451A793A1EE968A7B2128CC86FF9FCAE43C264C3F8'
   checksumType64 = 'sha256'
 
